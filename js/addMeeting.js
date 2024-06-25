2d34792115c9d41f604a5fa431dfe983bf87ec5a
i-takumi <22110013@oic-ok.ac.jp>
2024-06-25 10:53:36 +0900

    HTML,JS

--- js/addMeeting.js
+++ js/addMeeting.js
@@ -2,16 +2,19 @@ document.getElementById('addButton').onclick = function() {
     var mtgTitle = document.getElementById('mtgTitle').value;
     var mtgDate = document.getElementById('start').value;
     var mtgTime = document.getElementById('mtgTime').value;
-    var errorElement = document.getElementById('error');
+    var titleErrorElement = document.getElementById('titleError');
+    var datetimeErrorElement = document.getElementById('datetimeError');
     var defaultDate = "2024-07-01";
     var defaultTime = "12:00";
 
+    titleErrorElement.textContent = ""; // Clear any previous title error messages
+    datetimeErrorElement.textContent = ""; // Clear any previous datetime error messages
+
     if (mtgTitle === "") {
-        errorElement.textContent = "会議名を入力してください。";
+        titleErrorElement.textContent = "会議名を入力してください。";
     } else if (mtgDate === defaultDate && mtgTime === defaultTime) {
-        errorElement.textContent = "日時を選択してください。";
+        datetimeErrorElement.textContent = "日時を選択してください。";
     } else {
-        errorElement.textContent = ""; // Clear any previous error messages
         window.location.href = 'meetingMenu.html';
     }
 };
