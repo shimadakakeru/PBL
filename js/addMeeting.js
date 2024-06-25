f753f3e3bde028b40da9b875668b9345cefbcef8
i-takumi <22110013@oic-ok.ac.jp>
2024-06-25 11:46:59 +0900

    会議作成画面JS変更

--- js/addMeeting.js
+++ js/addMeeting.js
@@ -1,31 +1,33 @@
-document.getElementById('addButton').onclick = function() {
-    var mtgTitle = document.getElementById('mtgTitle').value;
-    var mtgDate = document.getElementById('start').value;
-    var mtgTime = document.getElementById('mtgTime').value;
-    var titleErrorElement = document.getElementById('titleError');
-    var datetimeErrorElement = document.getElementById('datetimeError');
-    var defaultDate = "2024-07-01";
-    var defaultTime = "12:00";
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('addButton').onclick = function() {
+        var mtgTitle = document.getElementById('mtgTitle').value.trim(); // trim() を使って前後の空白を除去
+        var mtgDate = document.getElementById('start').value;
+        var mtgTime = document.getElementById('mtgTime').value;
+        var titleErrorElement = document.getElementById('titleError');
+        var datetimeErrorElement = document.getElementById('datetimeError');
+        var defaultDate = "2024-07-01";
+        var defaultTime = "12:00";
 
-    titleErrorElement.textContent = ""; // Clear any previous title error messages
-    datetimeErrorElement.textContent = ""; // Clear any previous datetime error messages
+        titleErrorElement.textContent = ""; // Clear any previous title error messages
+        datetimeErrorElement.textContent = ""; // Clear any previous datetime error messages
 
-    var hasError = false;
+        var hasError = false;
 
-    if (mtgTitle === "") {
-        titleErrorElement.textContent = "会議名を入力してください。";
-        hasError = true;
-    }
+        if (mtgTitle === "") {
+            titleErrorElement.textContent = "会議名を入力してください。";
+            hasError = true;
+        }
 
-    if (mtgDate === defaultDate && mtgTime === defaultTime) {
-        datetimeErrorElement.textContent = "日時を選択してください。";
-        hasError = true;
-    }
+        if (mtgDate === defaultDate && mtgTime === defaultTime) {
+            datetimeErrorElement.textContent = "日時を選択してください。";
+            hasError = true;
+        }
 
-    if (!hasError) {
-        window.location.href = 'meetingMenu.html';
-    } else {
-        // エラーがある場合、ページのトップにスクロール
-        window.scrollTo(0, 0);
-    }
-};
+        if (!hasError) {
+            window.location.href = 'meetingMenu.html';
+        } else {
+            // エラーがある場合、ページのトップにスクロール
+            window.scrollTo(0, 0);
+        }
+    };
+});
